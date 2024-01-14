916a3d2dabe100ab33692374e38b6dc882bc14ea
Gerd Faedtke <gerd.faedtke@icloud.com>
2024-01-14 14:35:50 +0100

    Initial Commit

---
+++ CitHubSourceControl/CitHubSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitHubSourceControlApp.swift
+//  CitHubSourceControl
+//
+//  Created by Gerd Faedtke on 14.01.24.
+//
+
+import SwiftUI
+
+@main
+struct CitHubSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
